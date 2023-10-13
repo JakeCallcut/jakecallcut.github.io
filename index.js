6908ec7f1c004d5051efe2694895238fd6cd9fba
Jake Callcut <jakecallcut123@gmail.com>
2023-10-13 11:34:57 +0100

    contact, about, and work links working and looking good

---
+++ index.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+  var dots = document.getElementsByClassName('dot');
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].style.display = 'none';
+  }
+
+  const about = document.getElementById('about');
+  const contact = document.getElementById('contact');
+  const work = document.getElementById('work');
+  
+  about.addEventListener('mouseover', function(){
+      dots[0].style.display = 'inline';
+  });
+
+  work.addEventListener('mouseover', function(){
+      dots[1].style.display = 'inline';
+  });
+
+  contact.addEventListener('mouseover', function(){
+      dots[2].style.display = 'inline';
+  });
+
+  about.addEventListener('mouseout', function(){
+      dots[0].style.display = 'none';
+  });
+
+  work.addEventListener('mouseout', function(){
+      dots[1].style.display = 'none';
+  });
+
+  contact.addEventListener('mouseout', function(){
+      dots[2].style.display = 'none';
+  });
+
+});
