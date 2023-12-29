aa8a0db7955bf937d8ad1eb280d568a1454dda84
Jake Callcut <jakecallcut123@gmail.com>
2023-12-29 15:21:17 +0000

    contact page done

--- index.js
+++ index.js
@@ -5,9 +5,43 @@ document.addEventListener('DOMContentLoaded', function() {
     dots[i].style.display = 'none';
   }
 
+  const ghCard = document.getElementById('githubCard');
+  const liCard = document.getElementById('linkedinCard');
+  const emCard = document.getElementById('emailCard');
+
+  const ghImage = document.getElementById('githubImage');
+  const liImage = document.getElementById('linkedinImage');
+  const emImage = document.getElementById('emailImage');
+
   const ghText = document.getElementById('githubText');
   const liText = document.getElementById('linkedinText');
   const emText = document.getElementById('emailText');
+
+  ghCard.addEventListener('mouseover', function(){
+    ghText.style.color = "#6CA1D2";
+    ghText.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)";
+  });
+  liCard.addEventListener('mouseover', function(){
+    liText.style.color = "#6CA1D2";
+    liText.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)";
+  });
+  emCard.addEventListener('mouseover', function(){
+    emText.style.color = "#6CA1D2";
+    emText.style.textShadow = "2px 2px 4px rgba(0, 0, 0, 0.5)";
+  });
+
+  ghCard.addEventListener('mouseout', function(){
+    ghText.style.color = "darkGray";
+    ghText.style.textShadow = "";
+  });
+  liCard.addEventListener('mouseout', function(){
+    liText.style.color = "darkGray";
+    liText.style.textShadow = "";
+  });
+  emCard.addEventListener('mouseout', function(){
+    emText.style.color = "darkGray";
+    emText.style.textShadow = "";
+  });
 
   const about = document.getElementById('about');
   const contact = document.getElementById('contact');
@@ -16,11 +50,9 @@ document.addEventListener('DOMContentLoaded', function() {
   about.addEventListener('mouseover', function(){
       dots[0].style.display = 'inline';
   });
-
   work.addEventListener('mouseover', function(){
       dots[1].style.display = 'inline';
   });
-
   contact.addEventListener('mouseover', function(){
       dots[2].style.display = 'inline';
   });
@@ -28,16 +60,11 @@ document.addEventListener('DOMContentLoaded', function() {
   about.addEventListener('mouseout', function(){
       dots[0].style.display = 'none';
   });
-
   work.addEventListener('mouseout', function(){
       dots[1].style.display = 'none';
   });
-
   contact.addEventListener('mouseout', function(){
       dots[2].style.display = 'none';
   });
-
-
-
 
 });
